a77e142a0c1028ceb6618a0be756d564e7879904
gio sulakadze <80586641+Sula000@users.noreply.github.com>
2023-05-17 10:58:39 +0400

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+let userName = "gio";
+let surName = "sulakadze";
+let age = 18
+let gender = "male";
+
+console.log(userName);
+console.log(surName);
+console.log(18);
+console.log(gender);
+
+
+let rem = 101 % 2;
+console.log(rem);
